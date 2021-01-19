48580786b1be9596d5cc6fffc52e29c3b946b734
Chris Hobday <hobday.chris@protonmail.com>
2021-01-19 05:58:51 -0500

    Change name BuildP+AppImage.sh to BuildFPMAppImage.sh

--- Scripts/BuildFPMAppImage.sh
+++ Scripts/BuildFPMAppImage.sh
@@ -1,5 +1,5 @@
 #!/bin/sh -e
-# BuildP+AppImage.sh
+# BuildFPMAppImage.sh
 
 # Install needed dolphin dependencies
 sudo apt install -y cmake pkg-config git wget libao-dev libasound2-dev libavcodec-dev libavformat-dev libbluetooth-dev libenet-dev libgtk2.0-dev liblzo2-dev libminiupnpc-dev libopenal-dev libpulse-dev libreadline-dev libsfml-dev libsoil-dev libsoundtouch-dev libswscale-dev libusb-1.0-0-dev libwxbase3.0-dev libwxgtk3.0-dev libxext-dev libxrandr-dev portaudio19-dev zlib1g-dev libudev-dev libevdev-dev libmbedtls-dev libcurl4-openssl-dev libegl1-mesa-dev libpng-dev qtbase5-private-dev libxxf86vm-dev libxmu-dev libxi-dev 
