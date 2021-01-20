e9ff78528d0d5827d6a78c7b2605b8f235eb4ef9
Chris Hobday <hobday.chris@protonmail.com>
2021-01-20 12:21:32 -0500

    Add argument passing for AppImage path

--- Scripts/CreateFPMDesktopFile.sh
+++ Scripts/CreateFPMDesktopFile.sh
@@ -1,23 +1,28 @@
- 
 #!/bin/sh -e
 # CreateFPMDesktopFile.sh
 
+# Check if given absolute path
+if [[ $1 == /* ]]; then # Absolute path
+  PATHTOAPPIMAGE=$1
+else # Relative path
+  PATHTOAPPIMAGE="$(pwd)/$1"
+fi
+
 FPMVERSION="2.2"
 FPMDESKTOPFILENAME="FasterProjectPlus-${FPMVERSION}"
-PWD=$(pwd)
-APPIMAGENAME="Faster_Project_M-${FPMVERSION}-x86_64.AppImage"
-APPIMAGELOCATION="${PWD}/${APPIMAGENAME}"
 
 # --- Create FPM desktop file
-sudo rm -f ~/.local/share/applications/${FPMDESKTOPFILENAME}.desktop # Remove existing desktop file if it exists
+rm -f ~/.local/share/applications/${FPMDESKTOPFILENAME}.desktop # Remove existing desktop file if it exists
 touch ~/.local/share/applications/${FPMDESKTOPFILENAME}.desktop
 echo "[Desktop Entry]
 Type=Application
 GenericName=Wii/GameCube Emulator
 Comment=Ishiiruka fork for SSBPM
-Exec=${APPIMAGELOCATION}
+Exec=${PATHTOAPPIMAGE}
 Categories=Emulator;Game;
 Icon=ishiiruka
 Keywords=ProjectM;Project M;ProjectPlus;Project Plus;Project+
 Name=Faster Project M" >> ~/.local/share/applications/${FPMDESKTOPFILENAME}.desktop
 # ---
+
+echo "~/.local/share/applications/${FPMDESKTOPFILENAME}.desktop created with ${PATHTOAPPIMAGE} path"
